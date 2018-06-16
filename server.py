7f14579105ed78c5df5da6c27f112e194373e3dd
Qasim Iqbal <me@qas.im>
2018-06-16 10:09:24 -0400

    Add approve/reject responses

---
+++ server.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from twilio.twiml.voice_response import VoiceResponse
+
+
+app = Flask(__name__)
+
+
+@app.route("/answer", methods=['GET', 'POST'])
+def answer():
+    """Respond to incoming buzzer requests"""
+
+    return approve_response()
+
+
+def approve_response():
+    """Response to approve a buzzer request"""
+
+    resp = VoiceResponse()
+    resp.play(digits='9', loop=16)
+
+    return str(resp)
+
+
+def reject_response():
+    """Response to reject a buzzer request"""
+
+    resp = VoiceResponse()
+    resp.hangup()
+
+    return str(resp)
+
+
+if __name__ == "__main__":
+    app.run()
