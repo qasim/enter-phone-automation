0768df84bd77d0797aedbd96796c0239b69df76b
Qasim Iqbal <me@qas.im>
2018-06-16 12:27:37 -0400

    Move approve and reject to separate endpoints

--- server.py
+++ server.py
@@ -5,24 +5,30 @@ from twilio.twiml.voice_response import VoiceResponse
 app = Flask(__name__)
 
 
-@app.route("/answer", methods=['GET', 'POST'])
+@app.route("/answer")
 def answer():
     """Respond to incoming buzzer requests"""
 
-    return approve_response()
-
-
-def approve_response():
-    """Response to approve a buzzer request"""
-
     resp = VoiceResponse()
-    resp.play('', digits='w9', loop=0)
+    resp.redirect('/approve')
 
     return str(resp)
 
 
-def reject_response():
-    """Response to reject a buzzer request"""
+@app.route("/approve")
+def approve():
+    """Approve a buzzer request"""
+
+    resp = VoiceResponse()
+    resp.play('', digits='w9')
+    resp.redirect('/approve')
+
+    return str(resp)
+
+
+@app.route("/reject")
+def reject():
+    """Reject a buzzer request"""
 
     resp = VoiceResponse()
     resp.hangup()
