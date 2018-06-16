c5d562ecf915eee6e7df61b63da0328b59d28298
Qasim Iqbal <me@qas.im>
2018-06-16 12:43:41 -0400

    Use HTTP redirect vs Twilio redirect

--- server.py
+++ server.py
@@ -1,32 +1,29 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 from twilio.twiml.voice_response import VoiceResponse
 
 
 app = Flask(__name__)
 
 
-@app.route("/answer")
+@app.route("/answer", methods=['GET', 'POST'])
 def answer():
     """Respond to incoming buzzer requests"""
 
-    resp = VoiceResponse()
-    resp.redirect('/approve')
-
-    return str(resp)
+    return redirect(url_for('approve'))
 
 
-@app.route("/approve")
+@app.route("/approve", methods=['GET', 'POST'])
 def approve():
     """Approve a buzzer request"""
 
     resp = VoiceResponse()
-    resp.play('', digits='w9')
-    resp.redirect('/approve')
+    resp.play('', digits='9')
+    resp.redirect(url_for('approve'))
 
     return str(resp)
 
 
-@app.route("/reject")
+@app.route("/reject", methods=['GET', 'POST'])
 def reject():
     """Reject a buzzer request"""
 
